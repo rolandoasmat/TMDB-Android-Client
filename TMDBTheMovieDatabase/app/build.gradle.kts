0e21f829a25df2e6a6a33fcc1f10d933235d821a
Rolando Asmat <rolandoasmat@gmail.com>
2025-05-07 19:25:37 -0500

    Added Hilt dependency

--- TMDBTheMovieDatabase/app/build.gradle.kts
+++ TMDBTheMovieDatabase/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -28,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
@@ -40,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation("com.google.dagger:hilt-android:2.56.1") // version needs to match the one in Project
+    ksp("com.google.dagger:hilt-android-compiler:2.56.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
